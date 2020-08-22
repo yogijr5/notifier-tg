d489ef6fdff0cba5920f966ae211360f32f5db15
The Robot Septc <36376127+yogijr5@users.noreply.github.com>
2020-08-22 11:51:22 +0800

    Add files via upload

--- ssh-log.sh
+++ ssh-log.sh
@@ -1,10 +1,11 @@
+ 
 #!/bin/bash
 # @contributor : Yogi Junior
 # @community : Zerobyte.ID | Bash.ID
 
 #### Setting TelegramBot ####
 apikey="1308022779:AAFHFx48r3PCfBn84aW3kfJ-nmTcDfo2iLw" # Api key telegram
-chatid="1322536950" # Chat id telegram
+chatid="1322536950" # Chat id telegram 
 #############################
 
 DATE_LOG=$(date "+%d %b %Y %H:%M")
@@ -17,7 +18,7 @@ if [ -n "$SSH_CLIENT" ]; then
   GETIP=$(echo $GETINFO | grep -Po '(?<=IP:</th><td>)[^<]*')
   if [ -z "$GETIP" ]; then
     # Login SSH menggunakan IP Lokal
-    curl -s "https://api.telegram.org/bot${apikey}/sendMessage?chat_id=${chatid}&parse_mode=Markdown&text=^^^ NOTIFIKASI LOGIN SSH ^^^%0d%0aUser : *${USER}*%0d%0aDate : *${DA$
+    curl -s "https://api.telegram.org/bot${apikey}/sendMessage?chat_id=${chatid}&parse_mode=Markdown&text=^^^ NOTIFIKASI LOGIN SSH ^^^%0d%0aUser : *${USER}*%0d%0aDate : *${DATE_LOG}*.%0d%0aIP : ${IP}%0d%0aPORT : ${PORT}" > /dev/null
     echo "${IP} : ${PORT}" >> $LOG
   else
     # Login SSH menggunakan IP Publik
@@ -27,7 +28,7 @@ if [ -n "$SSH_CLIENT" ]; then
     ISP=$(echo $GETINFO | grep -Po '(?<=ISP:</th><td>)[^<]*')
     LATITUDE=$(echo $GETINFO | grep -Po '(?<=Latitude:</th><td>)[^&]*')
     LONGITUDE=$(echo $GETINFO | grep -Po '(?<=Longitude:</th><td>)[^&]*')
-    curl -s "https://api.telegram.org/bot${apikey}/sendMessage?chat_id=${chatid}&parse_mode=Markdown&text=^^^ NOTIFIKASI LOGIN SSH ^^^%0d%0aUser : *${USER}*%0d%0aDate : *${DA$
+    curl -s "https://api.telegram.org/bot${apikey}/sendMessage?chat_id=${chatid}&parse_mode=Markdown&text=^^^ NOTIFIKASI LOGIN SSH ^^^%0d%0aUser : *${USER}*%0d%0aDate : *${DATE_LOG}*%0d%0aIP : ${IP}%0d%0aPORT : ${PORT}%0d%0aCity : *${CITY}*%0d%0aState : *${STATE}*%0d%0aCountry : *${COUNTRY}*%0d%0aISP : *${ISP}*%0d%0aLatitude : *${LATITUDE}*%0d%0aLongitude : *${LONGITUDE}*%0d%0aRead more : https://whatismyipaddress.com/ip/${IP}" > /dev/null
     echo "${IP} : ${PORT} | ${COUNTRY}" >> $LOG
     fi
 fi
